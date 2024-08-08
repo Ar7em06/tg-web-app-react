9eccf48ca5789c93e8192b47f76a544f621e7274
Ar7em06 <artempacan83@gmail.com>
2024-08-08 22:50:25 +0500

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -30,7 +30,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://localhost:8000', {
+        fetch('https://willowy-valkyrie-286c9a.netlify.app/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
